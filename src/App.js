c83f723da8cc16be28f1ac18eab98adea595cbf4
Kuba Sobecki <gosustyle@gmail.com>
2022-12-03 20:01:40 +0100

    Reset all files for the new course module

--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
-import { Fragment } from 'react';
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import Header from './components/Header';
-import Auth from './components/Auth';
-import Counter from './components/Counter';
-import UserProfile from './components/UserProfile';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 function App() {
-    // const isUserAuthenticated = useSelector(state => {
-    //     console.log(state);
-    //     return state.auth.isUserAuthenticated;
-    // });
-    const isUserAuthenticated = useSelector(
-        state => state.auth.isUserAuthenticated
-    );
-
-    return (
-        <Fragment>
-            {isUserAuthenticated && <Header />}
-            {isUserAuthenticated && <UserProfile />}
-            {!isUserAuthenticated && <Auth />}
-            {isUserAuthenticated && <Counter />}
-        </Fragment>
-    );
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
 }
 
 export default App;
